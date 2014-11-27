57cdb29fb3852e46ac158d369ee9db76f9634b3b
Michael Lazarski <michael@lazarski.me>
2014-11-27 02:01:43 +0100

    should work now

--- package.js
+++ package.js
@@ -1,13 +1,16 @@
 Package.describe({
   name: 'ganymed:material-button',
   summary: ' A Material Button for Meteor',
-  version: '0.1.0',
-  git: ' /* Fill me in! */ '
+  version: '0.0.2',
+  git: 'https://github.com/lampe/ganymed-material-button'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.0');
-  api.addFiles('ganymed:material-button.js');
+  api.use('templating');
+  api.addFiles('ganymed:material-button.html', "client");
+  api.addFiles('ganymed:material-button.css', "client");
+  api.addFiles('ganymed:material-button.js', "client");
 });
 
 Package.onTest(function(api) {
